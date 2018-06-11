ab4b3f7656544d91e3f8f690df6d70ee8477b650
leocrossman <lkcrossman@gmail.com>
2018-06-11 12:52:46 -0400

    finished bubble sort basic

--- bubble/bubblesort.js
+++ bubble/bubblesort.js
@@ -1,1 +1,16 @@
-function bubbleSort(array) {}
+function swap(array, i, j) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[j - 1] > array[j]) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
+}
--- bubble/bubblesort.spec.js
+++ bubble/bubblesort.spec.js
@@ -2,4 +2,22 @@ describe('Bubble Sort', function() {
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([]);
   });
+  it('handles an array of size 2', function() {
+    expect(bubbleSort([5, 4])).toEqual([4, 5]);
+  });
+  it('handles an array of size n', function() {
+    expect(bubbleSort([2, 4, 7, 3, 1, 5])).toEqual([1, 2, 3, 4, 5, 7]);
+    expect(bubbleSort([2, 4, 7, 3, 1, 5, 6, 10, 9, 8])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+    ]);
+  });
 });
